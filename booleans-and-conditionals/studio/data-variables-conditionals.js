639ecd5a04cbf2901c1cff80b02477b526b346f0
e-bold <enkhmunkh@att.net>
2024-03-27 15:29:57 -0500

    Chapter 5 Studio

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,6 +1,51 @@
 // Initialize Variables below
+let date = "Monday 2019-03-18";
+let time = "10:05:34 AM";
+let astronautCount = 7;
+let astronautStatus = "ready";
+let averageAstronautMassKg = 80.7;
+let crewMassKg = astronautCount * averageAstronautMassKg;
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
+let maximumMassLimit = 850000;
+let fuelTempCelsius = -225;
+let minimumFuelTemp = -300;
+let maximumFuelTemp = -150;
+let fuelLevel = "100%";
+let weatherStatus = "clear";
+let preparedForLiftoff = true;
+
+let failText = "Shut down the launch operations!!!";
+let readyText = "Ready to liftoff!"
+let liftoffText = (
+    "\All systems are a go! Initiating space shuttle launch sequence." +
+    "\n - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -" +
+    "\nDate: " + date + 
+    "\nTime: " + time +
+    "\nAstronout Count: " + astronautCount +
+    "\nCrew Mass: " + crewMassKg +" kg" +
+    "\nFuel Mass: " + fuelMassKg + " kg" +
+    "\nShuttle Mass: " + shuttleMassKg + " kg" +
+    "\nTotal Mass: " + totalMassKg + " kg" +
+    "\nFuel Temperature: " + fuelTempCelsius + " C" +
+    "\nWeather Status: " + weatherStatus +
+    "\n - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
 
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
+if (astronautCount > 7 ) {
+    console.log(failText);
+} else if (astronautStatus !== "ready") {
+    console.log(failText);
+} else if (totalMassKg >= maximumMassLimit) {
+    console.log(failText);
+} else if((fuelTempCelsius < -300) || (fuelTempCelsius > -150) ) {
+    console.log(failText);
+} else if (fuelLevel !== "100%") {
+    console.log(failText);
+} else if (weatherStatus !== "clear") {
+    console.log(failText);
+} else console.log(liftoffText);
 
 // add logic below to verify all astronauts are ready
 
