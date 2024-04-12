53cd07b83eafd540a7e025c079e0418abeec7d6d
e-bold <enkhmunkh@att.net>
2024-04-11 19:51:02 -0500

    Completed Chapter 14 Studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,42 @@
 
 let launchcode = {
-  
-}
+    organization: 'nonprofit',
+    executiveDirector: 'Jeff',
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput: launchOutput,
+};
+
+function launchOutput (number){
+    let answer = '';
+
+    if (number % 3 === 0 && number % 5 === 0 && number % 2 === 0){
+        answer = 'LaunchCode Rocks!'
+    };
+
+    if (number % 3 || number % 5 || number % 2) {
+        if (number % 3 === 0 && number % 5 === 0){
+            answer = 'Code Rocks!'
+        } else if (number % 2 === 0 && number % 3 === 0){
+            answer = "LaunchCode!";
+         } else if (number % 2 === 0 && number % 5 === 0){
+            answer = 'Launch Rocks! (CRASH!!!!)';
+         } else if (number % 2 === 0 ) {
+            answer = 'Launch!';
+        } else if (number % 3 === 0) {
+            answer = 'Code!';
+        } else if (number % 5 === 0){
+            answer = 'Rocks!';
+        } else (answer = "Rutabagas! That doesn't work.")
+    }
+    return answer
+};
+
 
 module.exports = launchcode;
 
+
+// if (number % 5 === 0) {
+//     answer = 'Rocks!';
+//     return answer;
+// };
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,57 @@
 // launchcode.test.js code:
 const launchcode = require('../index.js');
 
+// Write your unit tests here!
 describe("Testing launchcode", function(){
-
-  // Write your unit tests here!
-  
+  test('should have key called organization with the value of nonprofit', function(){
+    expect(launchcode.organization).toBe('nonprofit')
+  });
+  test('should have key called executiveDirector with value of Jeff', function(){
+    expect(launchcode.executiveDirector).toBe('Jeff')
+  });
+  test('should have key called percentageCoolEmployees with a value of 100', function(){
+    expect(launchcode.percentageCoolEmployees === 100).toBe(true)
+  });
+  test('should have key called programsOffered that stores an array of["Web Development", "Data Analysis", "Liftoff"]',function(){
+    expect(launchcode.programsOffered.length).toBe(3);
+    expect(launchcode.programsOffered[0]).toBe('Web Development');
+    expect(launchcode.programsOffered[1]).toBe("Data Analysis");
+    expect(launchcode.programsOffered[2]).toBe("Liftoff");
+  });
 });
+
+describe('Testing launchOutput', function() {
+  test('Test to make sure launchOutput is a method/function', function(){
+    expect(typeof(launchcode.launchOutput)).toBe('function')
+  });
+  test('should return "Launch!" when number entered is only divisible by 2', function(){
+    let result = launchcode.launchOutput(2);
+    expect(result).toBe('Launch!')
+  });
+  test('should return "Code!" when number entered is only divisible by 3', function(){
+    let result = launchcode.launchOutput(3);
+    expect(result).toBe('Code!')
+  });
+  test('should return "Rocks!" when number entered is only divisible by 5', function(){
+    let result = launchcode.launchOutput(5);
+    expect(result).toBe('Rocks!')
+  });
+  test('should return "LaunchCode!" when number entered is divisible by 2 and 3', function(){
+    let result = launchcode.launchOutput(12);
+    expect(result).toBe('LaunchCode!')
+  });
+  test('should return "Code Rocks!" when number entered is divisible by 3 and 5', function(){
+    let result = launchcode.launchOutput(15);
+    expect(result).toBe('Code Rocks!')
+  });
+  test('should return "Launch Rocks!" when number entered is divisible by 2 and 5', function(){
+    let result = launchcode.launchOutput(10);
+    expect(result).toBe('Launch Rocks! (CRASH!!!!)')
+  });
+  test('should return "Rutabagas! That doesnt work" when number entered is not divisible by 2,3 or 5', function(){
+    let result = launchcode.launchOutput(1.5);
+    expect(result).toBe("Rutabagas! That doesn't work.")
+  });
+  
+
+})
