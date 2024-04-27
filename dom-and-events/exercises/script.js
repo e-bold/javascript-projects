e43b4856bfd995534607ce90d19acf4615321f26
e-bold <enkhmunkh@att.net>
2024-04-27 14:43:00 -0500

    Chapter 23 Exercise

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,6 +4,18 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+     missionAbort.addEventListener('mouseover', event => {
+        event.target.style.backgroundColor = 'blue'
+        // let element = event.target
+        // element.innerHTML = missionAbort.style.backgroundColor = 'blue' 
+         
+     })
+     missionAbort.addEventListener("mouseout", function( event ) {
+        event.target.style.backgroundColor = "";
+     });
     
 }
 
