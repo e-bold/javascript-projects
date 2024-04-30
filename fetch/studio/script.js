69a77ca32e351c61f661a1aa21aea9b3ddf963b3
e-bold <enkhmunkh@att.net>
2024-04-29 20:05:09 -0500

    Completed CHapter 27 studio

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,35 @@
 //TODO: Add Your Code Below
+
+
+window.addEventListener("load",async function(){
+    let data = await fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        return response.json()
+
+    });
+    console.log(data);
+    const container = document.getElementById("container");
+    let htmlString = "";
+    
+    for(let i = 0; i < data.length; i++){
+        htmlString = htmlString + `
+        <div class="astronaut">
+    <div class="bio">
+        <h3>${data[i].firstName} ${data[i].lastName}</h3>
+        <ul>
+        <li>Hours in space: ${data[i].hoursInSpace}</li>
+        <li>Active: ${data[i].active}</li>
+        <li>Skills: ${data[i].skills}</li>
+        </ul>
+    </div>
+    <img class="avatar" src="${data[i].picture}">
+</div>
+        `
+    container.innerHTML = htmlString
+    }
+
+})
+
+
+
+
+
